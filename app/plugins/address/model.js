6083207f611ec6244f488d6ba48dfa89eeeba9d7
Oscar Paramo <oscarparamo@me.com>
2015-09-30 02:33:51 -0500

    Add address verification route.

--- app/plugins/address/model.js
+++ app/plugins/address/model.js
@@ -35,6 +35,20 @@ module.exports = {
     metadata: Joi.object().pattern(/^.{1,40}/, Joi.string().max(500)).max(20)
   }).or('name', 'company'),
 
+  verifyAddress: Joi.object().keys({
+    address_line1: Joi.string(),
+
+    address_line2: Joi.string(),
+
+    address_city: Joi.string(),
+
+    address_state: Joi.string().max(2),
+
+    address_zip: Joi.number(),
+
+    address_country: Joi.string().max(2)
+  }),
+
   query: Joi.object().keys({
     count: Joi.number().max(100),
 
